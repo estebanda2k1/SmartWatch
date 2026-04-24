538af1d9003d02a80cf4e430e9eb1126dba9da45
Esteban <118080125+estebanda2k1@users.noreply.github.com>
2026-04-24 16:57:12 -0500

    Elaboración de testing en Smart Wallet

--- src/test/java/SmartWallet/SmartWalletTest.java
+++ src/test/java/SmartWallet/SmartWalletTest.java
@@ -1,10 +1,8 @@
 package SmartWallet;
-
 import org.SmartWallet.SmartWallet;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartWalletTest {
