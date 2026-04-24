ac0f3c265316e20b76262d02403ceecb73c20ea8
Esteban <118080125+estebanda2k1@users.noreply.github.com>
2026-04-24 16:55:16 -0500

    Elaboración de testing en Smart Wallet

---
+++ src/main/java/org/SmartWallet/SmartWallet.java
@@ -0,0 +1,47 @@
+package org.SmartWallet;
+
+
+public class SmartWallet {
+    private String Usuario;
+    private double saldo;
+
+    public boolean isEstadoUsuario() {
+        return estadoUsuario;
+    }
+
+    private boolean estadoUsuario;
+    public SmartWallet(String Usuario) {
+        this.Usuario = Usuario;
+        this.saldo = 0;
+        this.estadoUsuario = true;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public boolean deposit(double amount){
+        if (amount < 0){
+            return false;
+        }
+        saldo += amount;
+        if (Usuario=="Standard"&& saldo>5000){
+            return false;
+        }
+        if(amount>100){
+            saldo=saldo+(amount*0.01);
+        }
+        return(true);
+    }
+    public boolean withdraw(double amount){
+        if(amount <=0 || saldo<amount){
+            return false;
+        }
+        saldo=saldo-amount;
+        if(saldo==0){
+            estadoUsuario=false;
+        }
+        return true;
+
+    }
+
+}
---
+++ src/test/java/SmartWallet/SmartWalletTest.java
@@ -0,0 +1,66 @@
+package SmartWallet;
+
+import org.SmartWallet.SmartWallet;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SmartWalletTest {
+        private SmartWallet wallet;
+        @BeforeEach
+            void arranque(){
+                wallet=new SmartWallet("Standard");
+        }
+        @Test
+            void testDeposit(){
+            assertTrue(wallet.deposit(50));
+            assertEquals(50,wallet.getSaldo());
+        }
+        @Test
+            void testDepositNegativo(){
+            assertFalse(wallet.deposit(-50));
+        }
+        @Test
+            void testDepositNoCashback(){
+            assertTrue(wallet.deposit(50));
+            assertEquals(50,wallet.getSaldo());
+        }
+        @Test
+            void testDepositExactly100(){
+            assertTrue(wallet.deposit(100));
+            assertEquals(100,wallet.getSaldo());
+        }
+        @Test
+            void testDepositCashback(){
+            assertTrue(wallet.deposit(200));
+            assertEquals(202,wallet.getSaldo());
+        }
+        @Test
+            void testDepositoExcedido(){
+            wallet.deposit(4999);
+            assertFalse(wallet.deposit(200));
+        }
+        @Test
+            void testWithdraw(){
+            wallet.deposit(500);
+            assertTrue(wallet.withdraw(200));
+            assertEquals(305,wallet.getSaldo());
+        }
+        @Test
+            void testWithdrawNegativo(){
+            assertFalse(wallet.withdraw(-50));
+        }
+        @Test
+            void testWithdrawSinSaldoSuficiente(){
+            wallet.deposit(100);
+            assertFalse(wallet.withdraw(200));
+        }
+        @Test
+            void testWithdrawEstadoCliente(){
+            wallet.deposit(200);
+            wallet.withdraw(202);
+            assertFalse(wallet.isEstadoUsuario());
+        }
+}
